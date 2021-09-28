8b962da86ea006c4c12c11206057e195ec35ecd8
Mrzaidi200092 <87273689+Mrzaidi200092@users.noreply.github.com>
2021-09-29 01:14:21 +0500

    Add files via upload

---
+++ if else new fle.s
@@ -0,0 +1,44 @@
+li x1,5 #x1=3
+
+li x2,8
+
+li x3,5
+
+li x4,6
+
+li x5,10
+
+
+LOOP:
+
+bgt x2,x1,TRUE
+
+j FALSE
+
+CON:
+
+addi x6,x7,1 
+
+addi x0,x0,1 #increment by 1
+
+bne x1,x2,
+LOOP
+
+
+#END
+
+##LOOP END
+
+TRUE:
+
+add x2,x3,x4
+
+j CON
+
+FALSE:
+
+add x3,x2,x4
+
+j CON
+
+END:
---
+++ multiplication using shift.s
@@ -0,0 +1,9 @@
+addi x1,x0,2  #x1=2
+
+addi x2,x0,8  #x2=8
+
+sub x3,x2,x1 #subtract x3=x2-x1
+
+slli x4,x1,3  #shift the value of x1 three times
+
+add x4,x4,x1  #adding 
