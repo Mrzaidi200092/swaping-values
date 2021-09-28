b97214ab24bb50b2b2722b5bf28deacd05831d0f
Mrzaidi200092 <87273689+Mrzaidi200092@users.noreply.github.com>
2021-09-29 01:09:43 +0500

    Add files via upload
    In this code I have implemented the concept of C++ language of IF-ELSE in assemble language.

---
+++ IF ELSE.s
@@ -0,0 +1,42 @@
+li x1,5 #x1=3
+
+li x2,8 #x2=8
+
+li x3,5
+
+li x4,6
+
+li x5,10
+
+
+LOOP:
+
+bgt x2,x1,TRUE
+
+j FALSE
+
+CON:
+
+addi x0,x0,1 #increment by 1
+
+bne x3,x4,
+LOOP
+
+
+#END
+
+##LOOP END
+
+TRUE:
+
+add x2,x3,x4
+
+j CON
+
+FALSE:
+
+add x3,x2,x4
+
+j CON
+
+END:
